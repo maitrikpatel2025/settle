b76ac82f1bccc30fb5ddb8145ed15f7f91d7064d
yezy <yezileliilomo@hotmail.com>
2019-02-16 13:43:17 +0300

    [Fix]Make IsAuthenticatedOrReadOnly default auth class

--- settle/settings.py
+++ settle/settings.py
@@ -110,7 +110,7 @@ REST_FRAMEWORK = {
     ],
 
     'DEFAULT_PERMISSION_CLASSES': [
-        'api.permissions.IsAuthenticatedOrReadOnly'
+        'rest_framework.permissions.IsAuthenticatedOrReadOnly'
     ],
 
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
