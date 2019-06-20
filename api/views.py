71f5679918486848af3ed4936d279216843a5789
yezy <yezileliilomo@hotmail.com>
2019-06-20 13:13:59 +0300

    :fire:Remove unused imports

--- api/views.py
+++ api/views.py
@@ -1,9 +1,7 @@
 import json
-import itertools
 
 from django.db.models import Value
 from rest_framework import viewsets
-from rest_framework.response import Response
 from django.shortcuts import get_object_or_404
 from django.contrib.auth.models import User, Group
 from django_restql.mixins import DynamicFieldsMixin
