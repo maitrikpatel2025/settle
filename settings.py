0de7d8e401615c9d5c51ee73903141ed7c071578
yezy <yezileliilomo@hotmail.com>
2019-10-20 01:47:55 +0300

    :bug:Fix MEDIA_ROOT on settings

--- settings.py
+++ settings.py
@@ -162,5 +162,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'  #this is for every static file within django application
-MEDIA_ROOT = '/home/yezy/Projects/personal/settle/media'
+MEDIA_ROOT = '/home/yezy/Projects/personal/settle/media/'
 MEDIA_URL = '/media/'
