1c1af3814db9f844374e5f987d6337a0f4550a9f
yezy <yezileliilomo@hotmail.com>
2020-01-06 13:52:38 +0300

    :rocket:Add heroku deployment requirements

--- settings.py
+++ settings.py
@@ -94,12 +94,6 @@ DATABASES = {
 ############## CORS configurations #################
 CORS_ORIGIN_ALLOW_ALL = True
 CORS_ALLOW_CREDENTIALS = True
-CORS_ORIGIN_WHITELIST = (
-    'localhost:3030',
-)
-CORS_ORIGIN_REGEX_WHITELIST = (
-    'localhost:3030',
-)
 ############ End of CORS Configs ###################
 
 REST_FRAMEWORK = {
