2101eb66483d18aa0af6999df77cae6e89d48824
yezy <yezileliilomo@hotmail.com>
2020-01-30 14:29:24 +0300

    :bug:Fix MEDEA_ROOT on local env

--- settings.py
+++ settings.py
@@ -176,5 +176,5 @@ STATIC_URL = '/static/'  #this is for every static file within django applicatio
 if 'PRODUCTION' in os.environ:
     MEDIA_ROOT = '/var/www/settle/media/'
 else:
-    MEDIA_ROOT = './media'
+    MEDIA_ROOT = './media/'
 MEDIA_URL = '/media/'
